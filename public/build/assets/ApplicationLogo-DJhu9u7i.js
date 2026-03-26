b653b3d628d2db6d86754cf32c55a4f61621c75b
ibrahim elshorbagy <e.mohmed55@gmail.com>
2026-03-26 18:47:21 +0200

    rebuild

---
+++ public/build/assets/ApplicationLogo-DJhu9u7i.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-C4HBzPdp.js";function e(t){return s.jsx(s.Fragment,{children:s.jsx("img",{className:"object-contain ",src:"/assets/settings/logo.webp",alt:""})})}export{e as A};
